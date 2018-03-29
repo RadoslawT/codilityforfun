8f777452eca688a2d123289b82713a608ac4cabb
RadoslawT <radoslawtomczynski@gmail.com>
2018-03-29 20:44:49 +0200

    Fix indentation

--- iterations/max_bin_gap.rb
+++ iterations/max_bin_gap.rb
@@ -1,25 +1,25 @@
 def max_bin_gap(n)
-	return 0 if n == 0
+  return 0 if n == 0
 
-	gaps = []
-	gap  = 0
+  gaps = []
+  gap  = 0
 
-	while n > 0
-		if n % 2 == 0
-		  gap += 1
-		else
-		  gaps.push(gap)
-		  gap = 0
-		end
+  while n > 0
+    if n % 2 == 0
+      gap += 1
+    else
+      gaps.push(gap)
+      gap = 0
+    end
 
-		n /= 2
-	end
+    n /= 2
+  end
 
-	gaps.max
+  gaps.max
 end
 
 def test_max_bin_gap
-	[0, 592, 20, 15].each do |n|
-	  puts "n: #{n} bin: #{n.to_s(2)} gap: #{max_bin_gap(n)}"
-	end
+  [0, 592, 20, 15].each do |n|
+    puts "n: #{n} bin: #{n.to_s(2)} gap: #{max_bin_gap(n)}"
+  end
 end
