e3b234bde1bdeea3dcc3b35a49ab9f8f371c5cc4
RadoslawT <radoslawtomczynski@gmail.com>
2018-03-30 18:23:03 +0200

    Add recursive function

--- iterations/max_bin_gap.rb
+++ iterations/max_bin_gap.rb
@@ -1,4 +1,4 @@
-def max_bin_gap(n)
+def max_bin_gap_while(n)
   return 0 if n == 0
 
   gaps = []
@@ -18,8 +18,35 @@ def max_bin_gap(n)
   gaps.max
 end
 
-def test_max_bin_gap
-  [0, 592, 20, 15].each do |n|
-    puts "n: #{n} bin: #{n.to_s(2)} gap: #{max_bin_gap(n)}"
+def max_bin_gap_rec(n)
+  return 0 if n == 0
+  bin_gap(n.to_s(2).split(''))
+end
+
+def bin_gap(bin_n)
+  return 0 unless bin_n
+
+  gap = 0
+
+  bin_n.each do |bit|
+    if bit == '0'
+      gap += 1
+    else
+      break;
+    end
+  end
+
+  [gap, bin_gap(bin_n[gap+1..-1])].max
+end
+
+def test_max_bin_gap_while
+  [0, 592, 20, 15, 262153].each do |n|
+    puts "n: #{n} bin: #{n.to_s(2)} gap: #{max_bin_gap_while(n)}"
+  end
+end
+
+def test_max_bin_gap_rec
+  [0, 592, 20, 15, 262153].each do |n|
+    puts "n: #{n} bin: #{n.to_s(2)} gap: #{max_bin_gap_rec(n)}"
   end
 end
